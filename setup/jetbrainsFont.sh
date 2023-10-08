4018c49deca2809db3fcf5ba458aed40254bf32e
Fabian Heuberger <elanduir@gmail.com>
2023-10-08 15:02:50 +0000

    added setupscript for jetbrainsfont

---
+++ setup/jetbrainsFont.sh
@@ -0,0 +1,5 @@
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir ~/.fonts/JetBrainsMono
+tar -xf JetBrainsMono.tar.xz -C ~/.fonts/JetBrainsMono
+fc-cache -fv
+rm -rf JetBrainsMono.tar.xz
