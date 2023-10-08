c1bda7403bd728a805920a870f9a584aba8d15a2
Fabian Heuberger <72166721+Elanduir@users.noreply.github.com>
2023-10-08 20:00:12 +0200

    Update jetbrainsFont.sh

--- setup/jetbrainsFont.sh
+++ setup/jetbrainsFont.sh
@@ -1,4 +1,5 @@
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir ~/.fonts
 mkdir ~/.fonts/JetBrainsMono
 tar -xf JetBrainsMono.tar.xz -C ~/.fonts/JetBrainsMono
 fc-cache -fv
