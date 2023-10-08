0133d5f3f6d241789cbe04e6b05e773ec6b3a2d6
Fabian Heuberger <elanduir@gmail.com>
2023-10-08 17:41:28 +0000

    added shortuct for rofi

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -333,8 +333,11 @@ globalkeys = gears.table.join(
               {description = "increase the number of columns", group = "layout"}),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
               {description = "decrease the number of columns", group = "layout"}),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
-              {description = "select next", group = "layout"}),
+    awful.key({ modkey,           }, "space", function () 
+		awful.spawn.with_shell("rofi -show drun -theme ~/.config/rofi/configuration.rasi &>> /tmp/rofi.log")
+    end,
+        {description = "run rofi in  drun mode", group = "launcher"}),
+
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
 
