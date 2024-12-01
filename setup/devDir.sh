61bc296d84b2dbaf0ae6148a5ffcedced8554fc2
Fabian Heuberger <elanduir@gmail.com>
2024-12-01 13:37:19 +0100

    adds dev setup

---
+++ setup/devDir.sh
@@ -0,0 +1,6 @@
+mkdir ~/dev/private
+mkdir ~/dev/axonivy
+
+cp ../config/gitconfig/.gitconfig ~/.gitconfig
+cp ../config/gitconfig/axonivy/.gitconfig ~/dev/axonivy/.gitconfig
+cp ../config/gitconfig/private/.gitconfig ~/dev/private/.gitconfig
---
+++ setup/ssh.sh
@@ -0,0 +1,1 @@
+cp ../config/ssh/config ~/.ssh/config
